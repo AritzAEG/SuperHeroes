350e5b8d3bf1f20fb83c6e0a1140709a142987b8
AritzAEG <aritz.garcia@ikasle.aeg.eus>
2023-03-16 23:17:56 +0100

    Autenticacion Google

--- src/components/Paginas/SignIn/SignIn.js
+++ src/components/Paginas/SignIn/SignIn.js
@@ -1,9 +1,31 @@
 import React from 'react';
+import {getAuth, signInWithPopup, GoogleAuthProvider} from 'firebase/auth';
+import initializeAuthentication from './firebase.init';
+import { useState } from 'react';
+
+initializeAuthentication();
+const provider = new GoogleAuthProvider();
 
 const SignIn = () => {
+    const [user, setUser] = useState({});
+    const handleGoogleSignIn = () => {
+        const auth = getAuth();
+    
+        signInWithPopup(auth, provider).then((result) => {
+            const user = result.user;
+            setUser(user);
+        })
+    }
+
     return (   
         <div>
-            <h1>SignIn</h1>
+            <button onClick={handleGoogleSignIn}>Google Sign In</button> <br />
+            {user.email && (
+                <div>
+                    <h2>Welcome {user.displayName}</h2>
+                    <img src={user.photoURL} alt="" />
+                </div>
+            )}
         </div>
     )
 }
---
+++ src/components/Paginas/SignIn/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAzQvBWJCzLu-OI3IrWTkeg7B_WtVFEI2M",
+  authDomain: "pivotal-essence-380618.firebaseapp.com",
+  projectId: "pivotal-essence-380618",
+  storageBucket: "pivotal-essence-380618.appspot.com",
+  messagingSenderId: "399793344056",
+  appId: "1:399793344056:web:f4943a108f08ab25e020cd"
+};
+
+export default firebaseConfig;
