29cdf651ad7905f7da39ff01e0216b9c1ec3c7f0
AritzAEG <aritz.garcia@ikasle.aeg.eus>
2023-03-21 20:44:27 +0100

    Estilos_Finales

--- src/components/Paginas/SuperHeroes/SuperHeroe.js
+++ src/components/Paginas/SuperHeroes/SuperHeroe.js
@@ -1,14 +1,16 @@
 import React from 'react'
-
+import './SuperHeroes.css'
 
 function SuperHeroe({title, imageSource, text}) {
   return (
     <div className='card text-center mt-4'>
-        <img src={imageSource} alt='' />
-        <div className='card-body'>
+      <div className='overflow'>
+        <img src={imageSource} alt='' className='card-img-top'/>
+      </div>
+      <div className='card-body'>
             <h4 className='card-title'>{title}</h4>
             <p className='card-text'>{text}</p>
-        </div>
+      </div>
     </div>
   )
 }
