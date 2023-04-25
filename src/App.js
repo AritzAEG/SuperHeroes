605a51c98a07bdff2425453b4870fda5666351f1
AritzAEG <aritz.garcia@ikasle.aeg.eus>
2023-04-25 21:05:33 +0200

    Cambio Pagina crearyeditar

--- src/App.js
+++ src/App.js
@@ -4,9 +4,10 @@ import BarraNavegacion from './components/Navegacion/BarraNavegacion';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './components/Paginas/Home/Home';
 import SuperHeroes from './components/Paginas/SuperHeroes/SuperHeroes';
-import CrearEditar from './components/Paginas/CrearEditar/CrearEditar';
+import PirncipalHeroes from './components/Paginas/CrearEditar/PrincipalHeroes';
 import SignIn from './components/Paginas/SignIn/SignIn';
 import 'bootstrap/dist/css/bootstrap.min.css'
+import PrincipalHeroes from './components/Paginas/CrearEditar/PrincipalHeroes';
 
 function App() {
   return (
@@ -15,9 +16,9 @@ function App() {
       <Routes>
         <Route path='/home' element={<Home />} />
         <Route path='/superheroes' element={<SuperHeroes />} />
-        <Route path='/crearyeditar' element={<CrearEditar />} />
+        <Route path='/crearyeditar' element={<PrincipalHeroes />} />
         <Route path='/signin' element={<SignIn />} />
-      </Routes>
+      </Routes> 
     </Router>
   );
 }
