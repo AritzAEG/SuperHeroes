136973e08bfc9674a3270a3c56eccefcbb855aa5
AritzAEG <aritz.garcia@ikasle.aeg.eus>
2023-05-02 17:01:41 +0200

    Redirigir a pagina principal

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import BarraNavegacion from './components/Navegacion/BarraNavegacion';
-import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate} from 'react-router-dom';
 import Home from './components/Paginas/Home/Home';
 import SuperHeroes from './components/Paginas/SuperHeroes/SuperHeroes';
 import SignIn from './components/Paginas/SignIn/SignIn';
@@ -13,6 +13,7 @@ function App() {
     <Router>
       <BarraNavegacion />
       <Routes>
+        <Route path='' element={<Navigate to="/home" />} />
         <Route path='/home' element={<Home />} />
         <Route path='/superheroes' element={<SuperHeroes />} />
         <Route path='/editarheroes/*' element={<PrincipalHeroes />} />
