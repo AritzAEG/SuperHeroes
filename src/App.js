aa4b16c0a2e95f0ca1e7f0a56ca800557a4e4e7e
AritzAEG <aritz.garcia@ikasle.aeg.eus>
2023-05-02 16:45:19 +0200

    Remove Material-ui Icons && Core

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import BarraNavegacion from './components/Navegacion/BarraNavegacion';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import Home from './components/Paginas/Home/Home';
 import SuperHeroes from './components/Paginas/SuperHeroes/SuperHeroes';
 import SignIn from './components/Paginas/SignIn/SignIn';
