b444af4cc950a70aa562dda51946f3df0f6a6699
AritzAEG <aritz.garcia@ikasle.aeg.eus>
2023-05-14 22:36:32 +0200

    Progreso Combate

--- src/components/Paginas/Combate/Combate.js
+++ src/components/Paginas/Combate/Combate.js
@@ -44,20 +44,72 @@ function Combate () {
     let vida1;
     let vida2;
 
+    let dado1D100;
+    let dado1D20;
+
+    let dañoNormal = 0;
+    let dañoCritico = 0;
+    let pifia = 0;
+
     comienzo1 = datos[0].int + datos[0].com;
     comienzo2 = datos[1].int + datos[1].com;
 
     vida1 = datos[0].str * 10;
     vida2 = datos[1].str * 10;
-    
-    if (comienzo1 > comienzo2)
-    {
 
+    console.log("La vida de "+ datos[0].nombre +" es: "+ vida1 +" y la vida de "+ datos[1].nombre +" es: "+ vida2);
+    
+    if (comienzo1 > comienzo2) {
+    
+      dado1D100 = Math.floor((Math.random() * (100 - 1 + 1)) + 1);
+      
+      if (dado1D100 <= datos[0].com) // EXITO
+      {
+        console.log("Empieza atacando "+ datos[0].nombre)
+        dado1D20 = Math.floor((Math.random() * (20 - 1 + 1)) + 1);
+
+        if (dado1D20 >= 3 && dado1D20 <= 17) // DAÑO NORMAL
+        {
+          dañoNormal = (datos[0].pow + datos[0].str) * dado1D20 / 100;
+          vida2 -= dañoNormal;
+          console.log("Ataque con daño Normal: "+ dañoNormal +" "+ datos[1].nombre+ "tiene de vida: "+ vida2)
+        }
+        else if (dado1D20 >= 18 && dado1D20 <= 20) //DAÑO CRITICO
+        {
+          dañoCritico = (datos[0].int * datos[0].dur) / 100; 
+          vida2 = vida2 - dañoNormal - dañoCritico;
+        }
+        else if (dado1D20 >= 1 && dado1D20 <= 2) //PIFIA
+        {
+          pifia = datos[0].spe;
+        }
+      }
+      else 
+      {
+      
+      }
     }
     else 
     {
+      console.log("Empieza atacando "+ datos[1].nombre)
+      dado1D20 = Math.floor((Math.random() * (20 - 1 + 1)) + 1);
 
-    }
+      if (dado1D20 >= 3 && dado1D20 <= 17) // DAÑO NORMAL
+      {
+        dañoNormal = (datos[1].pow + datos[1].str) * dado1D20 / 100;
+        vida1 -= dañoNormal;
+        console.log("Ataque con daño Normal: "+ dañoNormal +" "+ datos[1].nombre+ "tiene de vida: "+ vida1)
+      }
+      else if (dado1D20 >= 18 && dado1D20 <= 20) //DAÑO CRITICO
+      {
+        dañoCritico = (datos[1].int * datos[1].dur) / 100; 
+        vida1 = vida1 - dañoNormal - dañoCritico;
+      }
+      else if (dado1D20 >= 1 && dado1D20 <= 2) //PIFIA
+      {
+        pifia = datos[1].spe;
+      }
+    } 
   }
 
   return(
